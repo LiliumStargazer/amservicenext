d189fc2c9aa24c733c9321b5d2bc5021a4670b83
LiliumStargazer <129180301+LiliumStargazer@users.noreply.github.com>
2024-12-23 14:41:02 +0100

    temporally disabled env paths

--- app/lib/param-proto-loader.ts
+++ app/lib/param-proto-loader.ts
@@ -7,6 +7,9 @@ import fs from 'fs';
 let TCoreParam: protobuf.Type;
 let TCoreParamCE: protobuf.Type;
 
+//provvisorio
+
+
 const loadProtoFile = (protoPath: string): Promise<protobuf.Root> => {
     return new Promise((resolve, reject) => {
         if (!fs.existsSync(protoPath) || fs.lstatSync(protoPath).isDirectory()) {
@@ -30,8 +33,11 @@ const loadProtoFile = (protoPath: string): Promise<protobuf.Root> => {
     });
 };
 
-const loadProtoAndroid = loadProtoFile(path.resolve(process.env.PROTO_PATH_ANDROID || ''));
-const loadProtoCE = loadProtoFile(path.resolve(process.env.PROTO_PATH_CE || ''));
+//disabilitato temporaneamente
+// const loadProtoAndroid = loadProtoFile(path.resolve(process.env.PROTO_PATH_ANDROID || ''));
+// const loadProtoCE = loadProtoFile(path.resolve(process.env.PROTO_PATH_CE || ''));
+const loadProtoAndroid = loadProtoFile(path.resolve("./app/lib/params/TCoreParam.proto"));
+const loadProtoCE = loadProtoFile(path.resolve("./app/lib/params/param.proto"));
 
 export async function getParams(data: RawParams[], softwareType: 'android' | 'windows'): Promise<protobuf.Message<Record<string, unknown>>> {
     try {
