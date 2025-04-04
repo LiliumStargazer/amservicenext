2d3a48356a2bdc6aaf9ca06594b79719fb337cca
LiliumStargazer <129180301+LiliumStargazer@users.noreply.github.com>
2025-04-04 15:49:16 +0200

    added options logic

--- app/components/SelectParam.tsx
+++ app/components/SelectParam.tsx
@@ -1,15 +1,29 @@
 'use client'
 
-import React, { } from 'react';
+import React, {useEffect} from 'react';
+import {ParamList} from "@/app/types/types";
+import {convertTimeStampToDate} from "@/app/utils/utils";
 
 interface SelectParamProps {
     loading: boolean;
     IDParam: string;
-    handleOnChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-    paramIdList: React.ReactNode[];
+    handleOnChangeParam: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    rawIdList:ParamList[];
 }
 
-const SelectParam: React.FC<SelectParamProps>= ({loading, IDParam, handleOnChange, paramIdList}) => {
+const SelectParam: React.FC<SelectParamProps>= ({loading, IDParam, handleOnChangeParam, rawIdList}) => {
+    const [paramList, setParamList] = React.useState<React.ReactNode[]>([]);
+
+    useEffect(() => {
+        if (rawIdList.length > 0) {
+            const options = rawIdList.map((element: ParamList) => (
+                <option key={element.ID} value={element.ID}>
+                    Id: {element.ID} Data: {convertTimeStampToDate(parseInt(element.DataOra))}
+                </option>
+            ));
+            setParamList(options);
+        }
+    }, [rawIdList]);
 
     return (
         loading ? <div className="skeleton h-12 w-64"></div>
@@ -17,9 +31,9 @@ const SelectParam: React.FC<SelectParamProps>= ({loading, IDParam, handleOnChang
             <select
                 className="select select-bordered select-md max-w-min min-w-max z-auto"
                 value={IDParam}
-                onChange={handleOnChange}
+                onChange={handleOnChangeParam}
             >
-                {paramIdList}
+                {paramList}
             </select>
     );
 };
